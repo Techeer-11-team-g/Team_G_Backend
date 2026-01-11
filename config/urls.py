9ffabae916b3f6b4f1ebfb90ca365de98110a791
이정 <ijeong@ijeongs-MacBook-Pro.local>
2026-01-11 17:44:27 +0900

    이미지 업로드 및 업로드한 이미지 리스트 조회 api 구현

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,8 @@ URL configuration for config project.
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 def health_check(request):
@@ -16,4 +18,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', health_check, name='health_check'),
     path('', include('django_prometheus.urls')),  # /metrics endpoint
-]
+    path('', include('analyses.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
