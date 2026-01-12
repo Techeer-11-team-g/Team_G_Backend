e0c352b1cea5d86457de2be0b751cce666e12aa7
노연서 <c0ding01seo@gmail.com>
2026-01-12 23:13:23 +0900

    feat: 프로필 페이지-사용자 정보 조회 API 개발

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserOnboardingView
+from .views import UserOnboardingView, UserMeView
 
 urlpatterns = [
+    path("me", UserMeView.as_view()),
     path("me/onboarding", UserOnboardingView.as_view()),
 ]
