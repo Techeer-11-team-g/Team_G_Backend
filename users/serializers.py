7bc7f125d4ac22dae446b2884fd070df50691c97
노연서 <c0ding01seo@gmail.com>
2026-01-16 01:37:27 +0900

    fix: 온보딩 수정

--- users/serializers.py
+++ users/serializers.py
@@ -6,10 +6,22 @@ User = get_user_model()
 class UserOnboardingSerializer(serializers.ModelSerializer):
     # 프론트에서 user_email로 보낸다니 source를 이용해 매핑 가능
     user_email = serializers.EmailField(source='email', required=True)
+    
+    user_id = serializers.IntegerField(source='id', read_only=True)
+    user_name = serializers.CharField(source='username', read_only=True)
+    updated_at = serializers.DateTimeField(source='updated_at', read_only=True) 
 
     class Meta:
         model = User
-        fields = ['user_email', 'address', 'phone_number', 'payment']
+        fields = [
+            'user_email', 
+            'address', 
+            'phone_number', 
+            'payment', 
+            "user_id", 
+            "user_name", 
+            "updated_at"
+        ]
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(source='id', read_only=True)
