29439603ea57f60eb09bc245c769c142808bf28a
이규호 <kyuhosandi@naver.com>
2026-01-17 22:37:37 +0900

    feat : 사용자 필수 정보 등록 스웨거 5차 수정

--- users/serializers.py
+++ users/serializers.py
@@ -31,8 +31,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
 
 class UserOnboardingSerializer(serializers.ModelSerializer):
     # 프론트에서 user_email로 보낸다니 source를 이용해 매핑 가능
-    user_email = serializers.EmailField(source='email', required=True)
-    
+    user_email = serializers.EmailField(source='email', required=True) 
     user_id = serializers.IntegerField(source='id', read_only=True)
     user_name = serializers.CharField(source='username', read_only=True)
     updated_at = serializers.DateTimeField(read_only=True) 
@@ -40,13 +39,8 @@ class UserOnboardingSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'user_email', 
-            'address', 
-            'phone_number', 
-            'payment', 
-            "user_id", 
-            "user_name", 
-            "updated_at"
+            'user_email', 'address', 'phone_number', 
+            'payment', "user_id", "user_name", "updated_at"
         ]
 
 class UserProfileSerializer(serializers.ModelSerializer):
